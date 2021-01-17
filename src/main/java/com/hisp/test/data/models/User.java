5cfa7ee570d6f7f2c7d8e256360776db9152770f
Diao Diallo <diao_diallo@gn.jsi.com>
2021-01-17 16:13:37 +0000

    Create entity User, .properties configuration, create super user when the app start.

---
+++ src/main/java/com/hisp/test/data/models/User.java
@@ -0,0 +1,63 @@
+package com.hisp.test.data.models;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Collection;
+
+@Entity
+@Table(name = "USER")
+public class User implements Serializable, UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+    private String username;
+    private String password;
+    private String email;
+    public Integer getUserId() {
+        return userId;
+    }
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+    public String getUsername() {
+        return username;
+    }
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getEmail() {
+        return this.email;
+    }
+    public void setEmail(String password) {
+        this.password = email;
+    }
+}
